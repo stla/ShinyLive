662ac79a04b49f9c3092a94621a76fe9e62575d8
stla <laurent_step@yahoo.fr>
2023-12-01 10:02:13 +0100

    experiments

--- experiments/app.R
+++ experiments/app.R
@@ -2,9 +2,20 @@ library(shiny)
 
 ui <- fluidPage(
   tags$head(tags$script("test.js")),
-  fileInput("file", "Upload")
+  fileInput("file", "Upload"),
+  verbatimTextOutput("wd"),
+  verbatimTextOutput("file")
 )
 
-server <- function(input, output, session) {}
+server <- function(input, output, session) {
+  
+  output$wd <- renderPrint({
+    getwd()
+  })
+  
+  output$file <- renderPrint({
+    input$file
+  })
+}
 
 shinyApp(ui = ui, server = server)
